4ea8dad84b294c9dc8b189935089c2579ae71dfe
stan e <yi.hang@live.com>
2017-09-29 17:23:02 +0800

    添加项目文件。

---
+++ NetCorePublishTool/FormMain.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace NetCorePublishTool
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnSelectSourcePath = new System.Windows.Forms.Button();
+            this.txtInPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPubMode = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutPath = new System.Windows.Forms.TextBox();
+            this.btnSelectOutPath = new System.Windows.Forms.Button();
+            this.cmbRuntime = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.ckbRuntime = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCmd = new System.Windows.Forms.RichTextBox();
+            this.btnPubNow = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkDoc = new System.Windows.Forms.LinkLabel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectSourcePath
+            // 
+            this.btnSelectSourcePath.Location = new System.Drawing.Point(500, 23);
+            this.btnSelectSourcePath.Name = "btnSelectSourcePath";
+            this.btnSelectSourcePath.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectSourcePath.TabIndex = 0;
+            this.btnSelectSourcePath.Text = "选择";
+            this.btnSelectSourcePath.UseVisualStyleBackColor = true;
+            // 
+            // txtInPath
+            // 
+            this.txtInPath.Location = new System.Drawing.Point(13, 25);
+            this.txtInPath.Name = "txtInPath";
+            this.txtInPath.Size = new System.Drawing.Size(481, 21);
+            this.txtInPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "项目目录(.csproj文件,输入目录将检测是否存在项目文件)";
+            // 
+            // cmbPubMode
+            // 
+            this.cmbPubMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPubMode.FormattingEnabled = true;
+            this.cmbPubMode.Items.AddRange(new object[] {
+            "Release",
+            "Debug"});
+            this.cmbPubMode.Location = new System.Drawing.Point(13, 113);
+            this.cmbPubMode.Name = "cmbPubMode";
+            this.cmbPubMode.Size = new System.Drawing.Size(121, 20);
+            this.cmbPubMode.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "发布路径";
+            // 
+            // txtOutPath
+            // 
+            this.txtOutPath.Location = new System.Drawing.Point(13, 69);
+            this.txtOutPath.Name = "txtOutPath";
+            this.txtOutPath.Size = new System.Drawing.Size(481, 21);
+            this.txtOutPath.TabIndex = 8;
+            // 
+            // btnSelectOutPath
+            // 
+            this.btnSelectOutPath.Location = new System.Drawing.Point(500, 69);
+            this.btnSelectOutPath.Name = "btnSelectOutPath";
+            this.btnSelectOutPath.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectOutPath.TabIndex = 7;
+            this.btnSelectOutPath.Text = "选择";
+            this.btnSelectOutPath.UseVisualStyleBackColor = true;
+            // 
+            // cmbRuntime
+            // 
+            this.cmbRuntime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRuntime.FormattingEnabled = true;
+            this.cmbRuntime.Location = new System.Drawing.Point(140, 113);
+            this.cmbRuntime.Name = "cmbRuntime";
+            this.cmbRuntime.Size = new System.Drawing.Size(152, 20);
+            this.cmbRuntime.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(299, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "目标框架(暂时无效)";
+            this.label5.Visible = false;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(299, 113);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 20);
+            this.comboBox3.TabIndex = 12;
+            this.comboBox3.Visible = false;
+            // 
+            // ckbRuntime
+            // 
+            this.ckbRuntime.AutoSize = true;
+            this.ckbRuntime.Location = new System.Drawing.Point(141, 94);
+            this.ckbRuntime.Name = "ckbRuntime";
+            this.ckbRuntime.Size = new System.Drawing.Size(84, 16);
+            this.ckbRuntime.TabIndex = 14;
+            this.ckbRuntime.Text = "发布运行时";
+            this.ckbRuntime.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "发布模式";
+            // 
+            // txtCmd
+            // 
+            this.txtCmd.Location = new System.Drawing.Point(95, 171);
+            this.txtCmd.Name = "txtCmd";
+            this.txtCmd.ReadOnly = true;
+            this.txtCmd.Size = new System.Drawing.Size(490, 67);
+            this.txtCmd.TabIndex = 17;
+            this.txtCmd.Text = "";
+            // 
+            // btnPubNow
+            // 
+            this.btnPubNow.Location = new System.Drawing.Point(13, 171);
+            this.btnPubNow.Name = "btnPubNow";
+            this.btnPubNow.Size = new System.Drawing.Size(75, 67);
+            this.btnPubNow.TabIndex = 18;
+            this.btnPubNow.Text = "立即发布";
+            this.btnPubNow.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(227, 12);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "注:目标框架需要配置项目文件,详情查看:";
+            // 
+            // linkDoc
+            // 
+            this.linkDoc.AutoSize = true;
+            this.linkDoc.Location = new System.Drawing.Point(14, 156);
+            this.linkDoc.Name = "linkDoc";
+            this.linkDoc.Size = new System.Drawing.Size(359, 12);
+            this.linkDoc.TabIndex = 20;
+            this.linkDoc.TabStop = true;
+            this.linkDoc.Text = "https://docs.microsoft.com/en-us/dotnet/standard/frameworks";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(437, 12);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "注:工具只负责拼接发布命令,所有执行操作均为.NetCore SDK,目前只支持2.x版本";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 271);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.linkDoc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnPubNow);
+            this.Controls.Add(this.txtCmd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ckbRuntime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.cmbRuntime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOutPath);
+            this.Controls.Add(this.btnSelectOutPath);
+            this.Controls.Add(this.cmbPubMode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInPath);
+            this.Controls.Add(this.btnSelectSourcePath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(613, 310);
+            this.MinimumSize = new System.Drawing.Size(613, 310);
+            this.Name = "FormMain";
+            this.Text = "NETCorePublishTools";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectSourcePath;
+        private System.Windows.Forms.TextBox txtInPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPubMode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutPath;
+        private System.Windows.Forms.Button btnSelectOutPath;
+        private System.Windows.Forms.ComboBox cmbRuntime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.CheckBox ckbRuntime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtCmd;
+        private System.Windows.Forms.Button btnPubNow;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkDoc;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ NetCorePublishTool/FormMain.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetCorePublishTool
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+
+
+            //Test();
+            this.Load += FormMain_Load;
+
+
+            this.btnSelectSourcePath.Click += BtnSelectSourcePath_Click;
+            this.btnSelectOutPath.Click += BtnSelectOutPath_Click;
+            this.btnPubNow.Click += BtnPubNow_Click;
+
+            this.txtInPath.TextChanged += TxtInPath_TextChanged;
+
+            this.linkDoc.LinkClicked += LinkDoc_LinkClicked;
+            this.ckbRuntime.CheckedChanged += CkbRuntime_CheckedChanged;
+            this.cmbRuntime.SelectedIndexChanged += CmbRuntime_SelectedIndexChanged;
+        }
+
+
+
+
+
+        /// <summary>
+        /// 窗体加载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.cmbPubMode.SelectedIndex = 0;
+
+            var os = File.ReadAllLines("./os.txt");
+            cmbRuntime.Items.AddRange(os);
+        }
+
+
+
+        bool isBtnSelectInPath = false;
+        /// <summary>
+        /// 选择项目文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSelectSourcePath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "项目文件|*.csproj";
+            ofd.Multiselect = false;
+            ofd.Title = "选择.Net Core项目文件";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtInPath.Text = ofd.FileName;
+                isBtnSelectInPath = true;
+            }
+        }
+
+        /// <summary>
+        /// 项目路径输入框改变
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TxtInPath_TextChanged(object sender, EventArgs e)
+        {
+            // 不是通过选择按钮执行的
+            if (!isBtnSelectInPath)
+            {
+                // 如果是目录则检测目录下是否存在.csproj文件
+                if (!txtInPath.Text.EndsWith(".csproj") && Directory.Exists(txtInPath.Text))
+                {
+                    var files = Directory.GetFiles(txtInPath.Text);
+                    // 遍历检测
+                    bool flag = false;
+                    foreach (var item in files)
+                    {
+                        if (item.EndsWith(".csproj"))
+                        {
+                            flag = true;
+                            txtOutPath.Focus();
+
+                            if (txtInPath.Text.EndsWith("\\"))
+                                txtInPath.Text = item;
+                            else
+                                txtInPath.Text = item;
+                            break;
+                        }
+                    }
+
+                    // 是否检测到项目文件
+                    if (!flag)
+                    {
+                        MessageBox.Show("未找到项目文件!请检查是否存在.csproj项目文件!","提示",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                    }
+                }
+
+
+                isBtnSelectInPath = false;
+            }
+        }
+
+
+        /// <summary>
+        /// 选择输出目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSelectOutPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtOutPath.Text = fbd.SelectedPath;
+            }
+        }
+
+
+        /// <summary>
+        /// 目标框架官方文档
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LinkDoc_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("explorer.exe", linkDoc.Text);
+        }
+
+        /// <summary>
+        /// 运行时启用框
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CkbRuntime_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckbRuntime.Checked && cmbRuntime.SelectedIndex == -1)
+            {
+                cmbRuntime.SelectedIndex = 1;
+            }
+        }
+
+        /// <summary>
+        /// 运行时选择框
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CmbRuntime_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbRuntime.SelectedIndex != -1)
+            {
+                if (cmbRuntime.SelectedItem.ToString().StartsWith("---"))
+                {
+                    cmbRuntime.SelectedIndex += 1;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 立即发布
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnPubNow_Click(object sender, EventArgs e)
+        {
+            #region 校验数据
+
+            txtInPath.Text = txtInPath.Text.Trim();
+            txtOutPath.Text = txtOutPath.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(txtInPath.Text)
+                || string.IsNullOrWhiteSpace(txtOutPath.Text))
+            {
+                MessageBox.Show("请选择项目文件和输出目录!", "提示");
+                return;
+            }
+
+            if (!Directory.Exists(txtOutPath.Text))
+            {
+                Directory.CreateDirectory(txtOutPath.Text);
+            }
+
+            #endregion
+
+
+            #region 拼接命令
+
+            var sb = new StringBuilder();
+            sb.Append("dotnet publish ");
+
+            sb.Append("\"");
+            sb.Append(txtInPath.Text);
+            sb.Append("\"");
+
+            sb.Append(" -c ");
+            sb.Append(cmbPubMode.SelectedItem.ToString());
+
+            if (ckbRuntime.Checked)
+            {
+                sb.Append(" -r ");
+                sb.Append(cmbRuntime.SelectedItem.ToString());
+            }
+
+            sb.Append(" -o ");
+            sb.Append("\"");
+            sb.Append(txtOutPath.Text);
+            sb.Append("\"");
+
+            txtCmd.Text = sb.ToString();
+
+            #endregion
+
+
+
+            RunCmd(txtCmd.Text);
+        }
+
+        /// <summary>
+        /// cmd执行命令
+        /// </summary>
+        /// <param name="cmdTxt">命令</param>
+        private void RunCmd(string cmdTxt)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;        //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;   //接受来自调用程序的输入信息
+            // p.StartInfo.RedirectStandardOutput = true;  //由调用程序获取输出信息
+            // p.StartInfo.RedirectStandardError = true;   //重定向标准错误输出
+            // p.StartInfo.CreateNoWindow = true;          //不显示程序窗口
+            p.Start();//启动程序
+
+            //向cmd窗口写入命令
+            p.StandardInput.WriteLine(cmdTxt);
+            //p.StandardInput.AutoFlush = true;
+            p.Close();
+        }
+
+
+
+        private void Test()
+        {
+            List<String> Dlls = new List<string>();
+            List<String> Namespaces = new List<string>();
+            List<String> Methods = new List<string>();
+            foreach (var Assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (!Dlls.Contains(Assembly.GetName().Name))
+                    Dlls.Add(Assembly.GetName().Name);
+
+                foreach (var Type in Assembly.GetTypes())
+                {
+                    if (!Namespaces.Contains(Type.Namespace))
+                        Namespaces.Add(Type.Namespace);
+                    foreach (var Method in Type.GetMethods())
+                    {
+                        Methods.Add(String.Format("{0}.{1}", Type.Name, Method.Name));
+                    }
+                }
+
+            }
+        }
+    }
+}
